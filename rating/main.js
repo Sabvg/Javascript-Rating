70e71124d95ae03ea0b295b1b80189668c78ad4d
Sabvg <Sabela.vazquez.gago@gmail.com>
2020-10-12 21:39:23 +0200

    new rating

---
+++ rating/main.js
@@ -0,0 +1,45 @@
+var catOne = document.getElementById('cat-1');
+var catTwo = document.getElementById('cat-2');
+var catThree = document.getElementById('cat-3');
+var catFour = document.getElementById('cat-4');
+var catFive = document.getElementById('cat-5');
+
+
+
+function color(position) {
+    for (i = position; i > 0; i--) {
+        document.getElementById("cat-" + i).classList.add("color");
+    }
+
+    for (i = position + 1; i <= 5; i++) {
+        document.getElementById("cat-" + i).classList.remove("color");
+    }
+}
+
+
+catOne.onclick = function() {
+    color(1);
+};
+catTwo.onclick = function() {
+    color(2);
+};
+catThree.onclick = function() {
+    color(3);
+};
+catFour.onclick = function() {
+    color(4);
+};
+catFive.onclick = function() {
+    color(5);
+};
+
+
+
+document.addEventListener("click", (e) => {
+    let iconCat = document.getElementById("cats");
+    let targetCats = e.target;
+
+    if (!iconCat.contains(targetCats)) {
+        color(0);
+    }
+})
